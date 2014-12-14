7044cd1ddd49d8205885830acba1872b6dad8ba9
sabren <michal.wallace@gmail.com>
2014-12-14 06:01:34 -0600

    added keypressed

--- libcrt.pas
+++ libcrt.pas
@@ -11,7 +11,11 @@ procedure kvm_fg(i:byte); stdcall;
   begin textcolor(i); if i > 8 then highvideo else lowvideo
   end;
 
-procedure kvm_bg(i:byte);       stdcall; begin textbackground(i) end; 
+function kvm_keypressed:byte; stdcall;
+  begin result := ord(crt.keypressed)
+  end;
+
+procedure kvm_bg(i:byte);       stdcall; begin textbackground(i) end;
 procedure kvm_sound(hz:word);   stdcall; begin crt.sound(hz) end;
 procedure kvm_nosound;          stdcall; begin crt.nosound end;
 procedure kvm_cursoron;         stdcall; begin crt.cursoron end;
@@ -26,9 +30,9 @@ function kvm_wherey:word;  stdcall; begin result := crt.wherey end;
 function kvm_readkey:char; stdcall; begin result := crt.readkey end;
 function kvm_xmax:word;    stdcall; begin result := crt.windmaxx end;
 function kvm_ymax:word;    stdcall; begin result := crt.windmaxy end;
-  
+
 exports
-  kvm_readln, kvm_fg, kvm_bg,
+  kvm_readln, kvm_fg, kvm_bg, kvm_keypressed,
   kvm_sound,  kvm_nosound, kvm_cursoron, kvm_cursoroff,
   kvm_clrscr, kvm_clreol, kvm_insline, kvm_delline, kvm_gotoxy,
   kvm_wherex, kvm_wherey, kvm_readkey, kvm_xmax, kvm_ymax;
